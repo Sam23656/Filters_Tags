8e0c9380dde89e771bf33cbc2e97c58b63033c20
Sam23656 <sem26m06@gmail.com>
2023-09-24 20:12:59 +0500

    Update

--- Filter_Tags/app/views.py
+++ Filter_Tags/app/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render
 
 
@@ -13,6 +15,11 @@ def show_index_page(request):
             {'name': 'Grape', 'category': 'Fruit'},
             {'name': 'Hamburger', 'category': 'Meat'},
             {'name': 'Pepper', 'category': 'Vegetable'},
-        ]
+        ],
+        'float_value': 35.4241234,
+        'some_list': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        'date': datetime.datetime.now(),
+        'date_to': datetime.datetime(2025, 1, 1),
+        'url': 'https://www.example.org/foo?a=b&c=d'
     }
     return render(request, 'app/index.html', context=context)
